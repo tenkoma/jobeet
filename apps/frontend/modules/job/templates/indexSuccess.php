c2d2b42b1008463c53ffbb62983109cb541f2e1d
tenkoma <tenkoma@gmail.com>
2009-12-17 01:43:15 +0900

    カテゴリページへのリンクを追加

--- apps/frontend/modules/job/templates/indexSuccess.php
+++ apps/frontend/modules/job/templates/indexSuccess.php
@@ -7,9 +7,12 @@
         <div class="feed">
           <a href="">Feed</a>
         </div>
-        <h1><?php echo $category ?></h1>
+        <h1>
+            <?php echo link_to($category, 'category', $category) ?>
+        </h1>
       </div>
- 
+
+       
       <table class="jobs">
         <?php foreach ($category->getActiveJobs(sfConfig::get('app_max_jobs_on_homepage')) as $i => $job): ?>
           <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>">
@@ -25,6 +28,14 @@
           </tr>
         <?php endforeach; ?>
       </table>
+
+      <?php if (($count = $category->countActiveJobs() - sfConfig::get('app_max_jobs_on_homepage')) > 0): ?>
+        <div class="more_jobs">
+          and <?php echo link_to($count, 'category', $category) ?>
+          more...
+        </div>
+      <?php endif; ?>
+
     </div>
   <?php endforeach; ?>
 </div>
--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -11,15 +11,28 @@ class JobeetJobTable extends Doctrine_Table
 
   public function getActiveJobs(Doctrine_Query $q = null)
   {
+    return $this->addActiveJobsQuery($q)->execute();
+  }
+
+  public function countActiveJobs(Doctrine_Query $q = null)
+  {
+    return $this->addActiveJobsQuery($q)->count();
+  }
+
+  public function addActiveJobsQuery(Doctrine_Query $q = null)
+  {
     if (is_null($q))
     {
       $q = Doctrine_Query::create()
-              ->from('JobeetJob j');
+        ->from('JobeetJob j');
     }
 
-    $q->andWhere('j.expires_at > ?', date('Y-m-d H:i:s', time()))
-            ->addOrderBy('j.expires_at DESC');
-    
-    return $q->execute();
+    $alias = $q->getRootAlias();
+
+    $q->andWhere($alias . '.expires_at > ?', date('Y-m-d H:i:s', time()))
+      ->addOrderBy($alias . '.created_at DESC');
+
+    return $q;
   }
+
 }
