bc5ee4d2ed3fe01e63a75801952b024363dcaa26
tenkoma <tenkoma@gmail.com>
2009-12-31 21:33:51 +0900

    機能テストの追加

--- test/functional/frontend/categoryActionsTest.php
+++ test/functional/frontend/categoryActionsTest.php
@@ -3,13 +3,14 @@
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
 $browser = new sfTestFunctional(new sfBrowser());
+Doctrine::loadData(sfConfig::get('sf_test_dir').'/fixtures');
 
 $browser->
   get('/category/index')->
 
   with('request')->begin()->
     isParameter('module', 'category')->
-    isParameter('action', 'index')->
+    isParameter('action', 'show')->
   end()->
 
   with('response')->begin()->
--- test/functional/frontend/jobActionsTest.php
+++ test/functional/frontend/jobActionsTest.php
@@ -2,18 +2,43 @@
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
-$browser = new sfTestFunctional(new sfBrowser());
+$browser = new JobeetTestFunctional(new sfBrowser());
+$browser->loadData();
+$max = sfConfig::get('app_max_jobs_on_homepage');
 
-$browser->
-  get('/job/index')->
-
+$browser->info('1 - The homepage')->
+  get('/')->
   with('request')->begin()->
     isParameter('module', 'job')->
     isParameter('action', 'index')->
   end()->
-
   with('response')->begin()->
-    isStatusCode(200)->
-    checkElement('body', '!/This is a temporary page/')->
+    info('  1.1 - Expired jobs are not listed')->
+    checkElement('.jobs td.position:contains("expired")', false)->
+  end()
+;
+
+$browser->info('1 - The homepage')->
+  get('/')->
+  info(sprintf('  1.2 - Only %s jobs are listed for a category', $max))->
+  with('response')->
+    checkElement('.category_programming tr', 10)
+;
+
+$browser->info('1 - The homepage')->
+  get('/')->
+  info('  1.3 - A category has a link to the category page only if too many jobs')->
+  with('response')->begin()->
+    checkElement('.category_design .more_jobs', false)->
+    checkElement('.category_programming .more_jobs')->
+  end()
+;
+
+$browser->info('1 - The homepage')->
+  get('/')->
+  info('  1.4 - Jobs are sorted by date')->
+  with('response')->begin()->
+    checkElement(sprintf('.category_programming tr:first a[href*="/%d/"]',
+      $browser->getMostRecentProgrammingJob()->getId()))->
   end()
 ;
